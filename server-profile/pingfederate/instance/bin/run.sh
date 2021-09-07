d61551700188c5947d31e8d8a5825b15b6085096
leggettc <leggettc@gmail.com>
2021-09-07 15:46:48 -0400

    Update

--- server-profile/pingfederate/instance/bin/run.sh
+++ server-profile/pingfederate/instance/bin/run.sh
@@ -97,5 +97,6 @@ exec "${JAVA_HOME}"/bin/java ${JAVA_OPTS} ${JVM_OPTS} \
     -Dpf.java="${JAVA}" \
     -Dpf.java.opts="-Drun.properties=${run_props}" \
     -Dpf.classpath="${PF_CLASSPATH}" \
+    -Djavax.net.debug=ssl,handshake \
     -classpath "${PF_CLASSPATH}" \
     org.pingidentity.RunPF "${@}"
